6cdeb2a32d24da647e02a80c9c7a09456f9e05a0
wooyoon <dndbs14@gmail.com>
2022-09-22 20:28:05 +0900

    feat: 계좌리스트 계좌번호 마스킹
    정규표현식 이용해 앞뒤 2자리씩 남기고 '*'로 마스킹

--- src/pages/AccountList/components/AccountItem.jsx
+++ src/pages/AccountList/components/AccountItem.jsx
@@ -9,21 +9,24 @@ const AccountItem = ({ account }) => {
     status, //운용중, accountStatus.json 를 참조하여 실제 이름으로 보여져야 합니다.
     number, //앞 뒤 각각 두글자를 제외하고 나머지는 글자수에 맞게 * 글자로 마스킹 처리가 필요합니다.
     name,
-    assets, 
+    assets,
     payments,
     is_active,
     created_at,
     //updated_at
   } = account;
 
-  function applyCommas(number){
+  function applyCommas(number) {
     return number.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  }
+  function accountNumberMasking(number){
+    return number.replace(/(?<=\d{2})\d(?=\d{2})/g,'*');
   }
   return (
     <tr>
       <td>{uuid}</td>
       <td>{broker_id}</td>
-      <td>{number}</td>
+      <td>{accountNumberMasking(number)}</td>
       <td>{status}</td>
       <td>{name}</td>
       <td>{applyCommas(assets)}</td>
