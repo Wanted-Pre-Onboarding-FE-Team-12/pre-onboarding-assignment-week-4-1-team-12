288f76dfd35546d021eacc6c5507c8e84f872238
jiye-7 <jy522hi@gmail.com>
2022-09-22 22:41:11 +0900

    fix: account list
    - 자산 금액 계산에 따른 스타일 적용
    - 계좌번호 마스킹 처리 정규표현식 수정

--- src/pages/AccountDetail/index.jsx
+++ src/pages/AccountDetail/index.jsx
@@ -1,8 +1,113 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getAccountDetail } from '@api/accountApi';
 import Layout from '@layout/index';
+import styled from 'styled-components';
+
+/**
+ * 서버로 상세 요청 보내기
+ * 받은 정보들 화면에 렌더링
+ */
 
 const AccountDetail = () => {
-  return <Layout>AccountDetail</Layout>;
+  const location = useLocation();
+  const accountId = +location.pathname.split('/')[2];
+
+  // server 요청 보내고 받은 응답 데이터 state에 저장
+  const [account, setAccount] = useState([]);
+  const {
+    assets,
+    payments,
+    user_id,
+    is_active,
+    broker_id,
+    name,
+    number,
+    status,
+    created_at,
+    updated_at,
+  } = account;
+
+  // useEffect로 바로 요청 보내고 state update 처리
+  useEffect(() => {
+    const getAccountDetailData = async () => {
+      const payload = await getAccountDetail(accountId);
+      if (payload) {
+        setAccount({ ...payload });
+      }
+    };
+    getAccountDetailData();
+  }, []);
+
+  return (
+    <Layout>
+      <AccountDetailContainer>
+        <h1>계좌 정보</h1>
+        <div>
+          <span>고객 이름</span>
+          <span>{user_id}</span>
+        </div>
+        <div>
+          <span>은행 명</span>
+          <span>{broker_id}</span>
+        </div>
+        <div>
+          <span>계좌 번호</span>
+          <span>{number}</span>
+        </div>
+        <div>
+          <span>계좌 상태</span>
+          <span>{status}</span>
+        </div>
+        <div>
+          <span>계좌명</span>
+          <span>{name}</span>
+        </div>
+        <div>
+          <span>평가 금액</span>
+          <span>{assets}</span>
+        </div>
+        <div>
+          <span>입금 금액</span>
+          <span>{payments}</span>
+        </div>
+
+        <div>
+          <span>계좌 활성화 여부</span>
+          <span>{is_active}</span>
+        </div>
+        <div>
+          <span>계좌 계설일</span>
+          <span>{created_at}</span>
+        </div>
+        <div>
+          <span>계좌 변경일</span>
+          <span>{updated_at}</span>
+        </div>
+      </AccountDetailContainer>
+    </Layout>
+  );
 };
 
 export default AccountDetail;
+
+const AccountDetailContainer = styled.div`
+  background-color: gray;
+`;
+
+/**
+    <thead style={{ fontSize: '14px', marginRight: '1.5rem' }}>
+      <tr>
+        {account?.map((title, idx) => (
+          <th key={idx}>{title[0]}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody style={{ display: 'flex' }}>
+      {account?.map((item, idx) => (
+        <tr key={idx}>
+          <td>{item[1]}</td>
+        </tr>
+      ))}
+    </tbody>
+ */
--- src/pages/AccountList/ListItem/index.jsx
+++ src/pages/AccountList/ListItem/index.jsx
@@ -2,21 +2,52 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { makeThousandSeparator, makeAccountNumberMasking } from '@utils/account';
 import styled from 'styled-components';
+import { useCallback, useEffect, useState } from 'react';
+
+/**
+ * 총 자산 금액 계산 후 보여지는 조건
+ * - 수익률(평가 금액)이 플러스인 계좌의 총자산 금액은 빨간색
+ * - 마이너스일 경우 파란색
+ * - 원금(입금 금액)과 동일한 경우 검정색으로 보여줘야 한다.
+ */
 
 const ListItem = ({ account, userIdHashObj, accountStatusHashObj }) => {
   const { broker_id, id, user_id, status, number, name, assets, payments, is_active, created_at } =
     account;
   const navigate = useNavigate();
   const { brokerList } = useSelector(({ account }) => account);
+  const [assetInfo, setAssetInfo] = useState('');
+
+  const makeAssetAssessment = useCallback(() => {
+    const asset = parseInt(assets.split('.')[0]);
+    const payment = parseInt(payments.split('.')[0]);
+
+    let assetResult = '';
+
+    if (asset > payment) {
+      assetResult = '#b71c1c';
+    } else if (assets < payment) {
+      assetResult = '#1976d2';
+    } else if (asset === payment) {
+      assetResult = '#212121';
+    }
+    setAssetInfo(assetResult);
+  }, [assets, payments]);
+
+  useEffect(() => {
+    makeAssetAssessment(assets, payments);
+  }, [makeAssetAssessment, assets, payments]);
 
   return (
     <AccountListContainer>
       <p>{brokerList[broker_id] ?? '-'}</p>
-      <p onClick={() => navigate(`/accounts/${id}`)}>{makeAccountNumberMasking(number) ?? '-'}</p>
+      <AccountInfo onClick={() => navigate(`/accounts/${id}`)}>
+        {makeAccountNumberMasking(number) ?? '-'}
+      </AccountInfo>
       <p onClick={() => navigate(`/users/${user_id}`)}>{userIdHashObj[user_id] ?? '-'} </p>
       <p>{accountStatusHashObj[status] ?? '-'}</p>
       <p>{name}</p>
-      <p>{makeThousandSeparator(assets)}</p>
+      <p style={{ color: assetInfo }}>{makeThousandSeparator(assets)}</p>
       <p>{makeThousandSeparator(payments)}</p>
       <p>{is_active ? 'O' : 'X'}</p>
       <p>{created_at.split('T')[0]}</p>
@@ -41,4 +72,8 @@ const AccountListContainer = styled.div`
     text-align: center;
     font-size: 14px;
   }
+`;
+
+const AccountInfo = styled.p`
+  color: #607d8b;
 `;
