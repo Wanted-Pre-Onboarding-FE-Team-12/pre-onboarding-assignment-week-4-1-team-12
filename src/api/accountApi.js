7a80062eb486eb4c7c767ae7495a7413401b26b8
jiye-7 <62678492+jiye-7@users.noreply.github.com>
2022-09-24 06:28:52 +0900

    feature: account page (#20)
    * chore: tailwind setting
    
    * feature: 필요한 정보들 서버에 요청, 리듀서에 저장
    
    - accounts
    - accountStatus
    - broker
    - brokerFormat
    - users
    
    * feature: account list
    
    - 고객 id가 같은 값을 찾아 고객명으로 보이도록 처리
    - 브로커명 실제 이름으로 보이도록 처리
    - 계좌 상태 실제이름으로 보이도록 처리
    - 평가금액, 입금금액 3자리 마다 , 처리
    - 계좌 개설일 YYYY-MM-DD로 처리
    
    * feature: account list 표기 정보 추가 기능 개발
    
    - 계좌번호 앞, 뒤 각 2자리 제외 '*' 마스킹 처리
    - 고객명 클릭 시 사용자 상세화면으로 이동
    - 계좌번호 클릭시 계좌정보 상세화면으로 이동
    
    * feature: header page title 함수로 분리
    
    - pathname에 따라 페이지 제목 변경되는 모듈 분리
    
    * fix: 주석 제거
    
    * fix: account list
    
    - 자산 금액 계산에 따른 스타일 적용
    - 계좌번호 마스킹 처리 정규표현식 수정
    
    * feature: account detail page
    
    - 고객이름, 은행명, 계좌 상태 값 처리
    - 전반적인 데이터 렌더링
    (스타일은 추후 적용)
    
    * feature: filtering
    
    - 증권사
    - 계좌 상태
    - 계좌 활성화 여부
    - 검색어에 따른 필터 처리
    
    * chore: 주석 삭제
    
    * feature: account page page nation
    
    - 필터, 검색어에 따른 페이지네이션
    - 전체 리스트 목록 페이지네이션 처리
    
    * feature: page nation 구현
    
    - error 발생시 toast message 뜨도록 수정
    - filtering에 따라 page 개수 설정
    
    * feature: loading
    
    - 공통 로딩 컴포넌트 추가
    - 서버 요청결과 success, fail 마다 loading 여부 처리
    
    * feature: code 수정, style 수정
    
    - 날짜 포맷팅처리하는 함수 분리
    
    * feature: account number formating
    
    - 증권사별 계좌 번호 포맷처리

--- src/api/accountApi.js
+++ src/api/accountApi.js
@@ -1,6 +1,46 @@
+import queryString from 'query-string';
 import { authInstance } from './index';
 
+/** account list */
 export const getAccountList = async () => {
   const response = await authInstance.get('/accounts');
+  return response;
+};
+
+/** account status */
+export const getAccountStatusList = async () => {
+  const response = await authInstance.get('/accountStatus');
+  return response;
+};
+
+/** broker list */
+export const getBrokerList = async () => {
+  const response = await authInstance.get('/brokers');
+  return response;
+};
+
+/** broker format */
+export const getBrokerFormatList = async () => {
+  const response = await authInstance.get('/brokerFormat');
+  return response;
+};
+
+/** account detail */
+export const getAccountDetail = async id => {
+  const response = await authInstance.get(`/accounts/${id}`);
+  return response.data;
+};
+
+/**
+ * filtering & search
+ * issue
+ * search 범위가 account list response data들에 한해서만 가능함.
+ *  ex) uuid가 검색 됨
+ *  ex) 한글 증권사 검색 불가능
+ *  ex) 고객명 한글 검색 불가, user_id로 검색 가능 (number)
+ * */
+export const getAccounts = async query => {
+  const queryStr = queryString.stringify(query, { skipEmptyString: true, skipNull: true });
+  const response = await authInstance.get(`/accounts?${queryStr}`);
   return response;
 };
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,6 +2,5 @@ import { authInstance } from './index';
 
 export const getUserList = async () => {
   const response = await authInstance.get('/users');
-  console.log(response);
   return response;
 };
