0e71c50471f755182ae2024d8ca397bf428030e3
jiye-7 <jy522hi@gmail.com>
2022-09-23 19:19:06 +0900

    feature: filtering
    - 증권사
    - 계좌 상태
    - 계좌 활성화 여부
    - 검색어에 따른 필터 처리

--- src/pages/AccountList/index.jsx
+++ src/pages/AccountList/index.jsx
@@ -7,7 +7,12 @@ import {
   getBrokerFormat,
   getUsers,
 } from '@store/modules/accountSlice';
+import { getAccountFilteringList } from '@api/accountApi';
 import AccountSubTitle from './AccountSubTitle';
+import AccountBankSelectOption from './AccountBankSelectOption';
+import AccountStatusSelectOption from './AccountStatusSelectOption';
+import AccountActiveSelectOption from './AccountActiveSelectOption';
+import AccountSearch from './AccountSearch';
 import ListItem from './ListItem';
 import Layout from '@layout/index';
 import styled from 'styled-components';
@@ -15,7 +20,14 @@ import styled from 'styled-components';
 const AccountList = () => {
   const dispatch = useDispatch();
   const [accounts, setAccounts] = useState([]);
-  const { userList, accountStatusList } = useSelector(({ account }) => account);
+  const { userList, accountStatusList, brokerList } = useSelector(({ account }) => account);
+  // 은행명, 계좌상태, 계좌 활성화 여부, 검색어 상태값
+  const [filteringOption, setFilteringOption] = useState({
+    selectBroker: '',
+    selectAccountState: '',
+    selectAccountIsActive: false,
+    searchQuery: '',
+  });
 
   const userIdHashObj = useMemo(() => {
     if (userList) {
@@ -33,6 +45,12 @@ const AccountList = () => {
     }
     return {};
   }, [accountStatusList]);
+
+  // 은행명, 계좌상태, 계좌 활성화 여부에 따른 state 업데이트 함수
+  const handleUpdateFilteringOption = (option, value) => {
+    // 업데이트 된 종류에 따라 state 업데이트
+    setFilteringOption({ ...filteringOption, [option]: value });
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -55,9 +73,57 @@ const AccountList = () => {
     getData();
   }, []);
 
+  /** select option들 값 변경 될 때 마다 필터링 요청 */
+  useEffect(() => {
+    const query = {
+      q: filteringOption.searchQuery,
+      broker_id: filteringOption.selectBroker,
+      status: filteringOption.selectAccountState,
+      is_active: filteringOption.selectAccountIsActive,
+    };
+
+    const getFilteringData = async () => {
+      try {
+        let response = await getAccountFilteringList(query);
+        console.log(response);
+        setAccounts([...response]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getFilteringData();
+  }, [filteringOption]);
+
   return (
     <Layout>
       <AccountListContainer>
+        <OptionContainerBox>
+          <div>
+            {/** 은행 선택 옵션 */}
+            <AccountBankSelectOption
+              brokerList={brokerList}
+              selectOptionKey="selectBroker"
+              handleUpdateFilteringOption={handleUpdateFilteringOption}
+            />
+            {/** 계좌 상태별 옵션 */}
+            <AccountStatusSelectOption
+              accountStatusList={accountStatusList}
+              selectOptionKey="selectAccountState"
+              handleUpdateFilteringOption={handleUpdateFilteringOption}
+            />
+            {/** 계좌 활성화 여부 옵션 */}
+            <AccountActiveSelectOption
+              selectOptionKey="selectAccountIsActive"
+              handleUpdateFilteringOption={handleUpdateFilteringOption}
+            />
+          </div>
+          <div>
+            <AccountSearch
+              selectOptionKey="searchQuery"
+              handleUpdateFilteringOption={handleUpdateFilteringOption}
+            />
+          </div>
+        </OptionContainerBox>
         <AccountSubTitle />
         {accounts?.map((account, idx) => (
           <ListItem
@@ -75,3 +141,16 @@ const AccountList = () => {
 export default AccountList;
 
 const AccountListContainer = styled.div``;
+
+const OptionContainerBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+
+  > div:first-child {
+    display: flex;
+  }
+`;
