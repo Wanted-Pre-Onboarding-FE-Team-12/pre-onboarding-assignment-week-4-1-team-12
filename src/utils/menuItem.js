dd2c3c240bbac6cab932ab254f04cf4b89eab229
KiHoon Roh <started677@gmail.com>
2022-09-25 01:26:40 +0900

    fix: path name 정규식 수정
    계좌 상세화면을 불러오지 못하는 이슈 해결.

--- src/utils/menuItem.js
+++ src/utils/menuItem.js
@@ -11,12 +11,13 @@ export const convertPageTitle = pathname => {
 
   if (pathname === '/accounts') {
     menuTitle = '계좌 목록';
-  } else if (pathname.match(/^\/accounts\/(\d+)$/)) {
+  } else if (pathname.match(/^\/accounts(\/\d)+$/)) {
     menuTitle = '계좌 상세화면';
   } else if (pathname === '/users') {
     menuTitle = '사용자 목록';
   } else if (pathname.match(/^\/users\/(\d+)$/)) {
     menuTitle = '사용자 상세화면';
   }
+
   return menuTitle;
 };
